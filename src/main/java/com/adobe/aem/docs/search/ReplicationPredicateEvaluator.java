97abb556a30b023895369568d2d4a8e931c55c47
Alexandre COLLIGNON <colligno@adobe.com>
2013-09-23 14:16:56 +0200

    Implement replication predicate evaluator logic

--- src/main/java/com/adobe/aem/docs/search/ReplicationPredicateEvaluator.java
+++ src/main/java/com/adobe/aem/docs/search/ReplicationPredicateEvaluator.java
@@ -20,10 +20,75 @@
 package com.adobe.aem.docs.search;
 
 import org.apache.felix.scr.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.day.cq.search.Predicate;
 import com.day.cq.search.eval.AbstractPredicateEvaluator;
+import com.day.cq.search.eval.EvaluationContext;
 
 @Component(metatype = false, factory = "com.day.cq.search.eval.PredicateEvaluator/repli")
 public class ReplicationPredicateEvaluator extends AbstractPredicateEvaluator {
-
+	static final String PE_NAME = "replic";
+    
+    static final String PN_LAST_REPLICATED_BY = "cq:lastReplicatedBy";
+    static final String PN_LAST_REPLICATED = "cq:lastReplicated";
+    static final String PN_LAST_REPLICATED_ACTION = "cq:lastReplicationAction";
+ 
+    static final String PREDICATE_BY = "by";
+    static final String PREDICATE_SINCE = "since";
+    static final String PREDICATE_SINCE_OP = " >= ";
+    static final String PREDICATE_ACTION = "action";
+ 
+    Logger log = LoggerFactory.getLogger(getClass());
+ 
+    /**
+     * Returns a XPath expression filtering by replication metadata.
+     *
+     * @see com.day.cq.search.eval.AbstractPredicateEvaluator#getXPathExpression(com.day.cq.search.Predicate,
+     *      com.day.cq.search.eval.EvaluationContext)
+     */
+    @Override
+    public String getXPathExpression(Predicate predicate,
+            EvaluationContext context) {
+ 
+        log.debug("predicate {}", predicate);
+ 
+        String date = predicate.get(PREDICATE_SINCE);
+        String user = predicate.get(PREDICATE_BY);
+        String action = predicate.get(PREDICATE_ACTION);
+         
+        StringBuilder sb = new StringBuilder();
+ 
+        if (date != null) {
+            sb.append(PN_LAST_REPLICATED).append(PREDICATE_SINCE_OP);
+            sb.append("xs:dateTime('").append(date).append("')");
+        }
+        if (user != null) {
+            addAndOperator(sb);
+            sb.append(PN_LAST_REPLICATED_BY);
+            sb.append("='").append(user).append("'");
+        }
+        if (action != null) {
+            addAndOperator(sb);
+            sb.append(PN_LAST_REPLICATED_ACTION);
+            sb.append("='").append(action).append("'");
+        }
+        String xpath = sb.toString();
+ 
+        log.debug("xpath **{}**", xpath);
+ 
+        return xpath;
+    }
+ 
+    /**
+     * Add an and operator if the builder is not empty.
+     *
+     * @param sb a {@link StringBuilder} containing the query under construction
+     */
+    private void addAndOperator(StringBuilder sb) {
+        if (sb.length() != 0) {
+            sb.append(" and ");
+        }
+    }
 }
